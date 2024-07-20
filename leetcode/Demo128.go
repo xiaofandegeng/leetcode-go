66e37a749825474cd9c255d2a08031edda3c8556
lhw <434962239@qq.com>
2024-07-20 22:29:38 +0800

    feature: 最长连续序列

--- leetcode/Demo128.go
+++ leetcode/Demo128.go
@@ -1,19 +1,22 @@
 package main
 
 func longestConsecutive(nums []int) int {
+	// 定义最大长度
 	maxLen := 0
+
+	// 将nums放入到一个map里面，如果存在就设为true
 	m := make(map[int]bool)
 
 	for _, num := range nums {
 		m[num] = true
 	}
 
-	for k, _ := range m {
-
+	for k := range m {
+		// 当当前值不存在比它小1的值，进入循环技术
 		if !m[k-1] {
+			// 设一个当前值和当前长度
 			curNum := k
 			curLen := 1
-
 			for m[curNum+1] {
 				curNum += 1
 				curLen += 1
@@ -23,6 +26,6 @@ func longestConsecutive(nums []int) int {
 			}
 		}
 	}
-
 	return maxLen
+
 }
