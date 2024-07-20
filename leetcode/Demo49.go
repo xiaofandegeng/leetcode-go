fd09f4fe86864e68618eee9f0500cb776d82ac82
lhw <434962239@qq.com>
2024-07-20 14:13:09 +0800

    feature: 字母异位词分组

--- leetcode/Demo49.go
+++ leetcode/Demo49.go
@@ -3,19 +3,23 @@ package main
 import "sort"
 
 func groupAnagrams(strs []string) [][]string {
-	mp := make(map[string][]string)
-	for _, str := range strs {
-		s := []byte(str)
-		sort.Slice(s, func(i, j int) bool {
-			return s[i] < s[j]
-		})
-		sortStr := string(s)
-		mp[sortStr] = append(mp[sortStr], str)
-	}
-	ans := make([][]string, 0, len(mp))
+	// 定义一个map接收排序后的str 和 string的数组
+	m := make(map[string][]string)
 
-	for _, v := range mp {
-		ans = append(ans, v)
+	for _, str := range strs {
+		// 将str转换为byte排序后再转换为string
+		bytes := []byte(str)
+		sort.Slice(bytes, func(i, j int) bool {
+			return bytes[i] < bytes[j]
+		})
+		tmp := string(bytes)
+		m[tmp] = append(m[tmp], str)
 	}
-	return ans
+	// 定义返回结果集合
+	res := make([][]string, 0, len(m))
+
+	for _, v := range m {
+		res = append(res, v)
+	}
+	return res
 }
