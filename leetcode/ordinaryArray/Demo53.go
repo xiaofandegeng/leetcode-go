93a0458a42001dc3bb88e6d9d94ccf988bd5fa8d
lhw <434962239@qq.com>
2024-07-25 20:27:29 +0800

    feature: 合并区间

--- leetcode/ordinaryArray/Demo53.go
+++ leetcode/ordinaryArray/Demo53.go
@@ -3,14 +3,14 @@ package ordinaryArray
 import "math"
 
 func maxSubArray(nums []int) int {
-	count := 0
 	maxCount := math.MinInt32
+
+	count := 0
 
 	for i := 0; i < len(nums); i++ {
 		num := nums[i]
 		count += num
 		maxCount = max(maxCount, count)
-
 		if count < 0 {
 			count = 0
 		}
